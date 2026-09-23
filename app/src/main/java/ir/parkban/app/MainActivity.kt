5d00d600e82b711e1d67e58f24e0802ffbbf9f14
arvinkhezri2231-jpg <arvin.khezri2231@gmail.com>
2026-09-23 20:25:59 +0330

    Refactor MainActivity to improve WebView initialization
    Refactored MainActivity to use a local WebView instance and simplified settings configuration.

--- app/src/main/java/ir/parkban/app/MainActivity.kt
+++ app/src/main/java/ir/parkban/app/MainActivity.kt
@@ -2,42 +2,23 @@ package ir.parkban.app
 
 import android.app.Activity
 import android.os.Bundle
-import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
 
-    private lateinit var webView: WebView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        webView = WebView(this)
-
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            databaseEnabled = true
-            loadWithOverviewMode = true
-            useWideViewPort = true
-            builtInZoomControls = false
-            displayZoomControls = false
-        }
+        val webView = WebView(applicationContext)
 
         webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
 
         setContentView(webView)
 
         webView.loadUrl("http://park-ban.ir/")
-    }
-
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
     }
 }
