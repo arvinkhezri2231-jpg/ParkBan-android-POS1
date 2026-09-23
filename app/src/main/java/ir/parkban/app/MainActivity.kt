906addb0e377b60ceaef3fe10d43b0f6fc16213e
arvinkhezri2231-jpg <arvin.khezri2231@gmail.com>
2026-09-23 20:19:04 +0330

    Update MainActivity.kt

--- app/src/main/java/ir/parkban/app/MainActivity.kt
+++ app/src/main/java/ir/parkban/app/MainActivity.kt
@@ -2,25 +2,42 @@ package ir.parkban.app
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
 
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
+        webView = WebView(this)
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.loadWithOverviewMode = true
-        webView.settings.useWideViewPort = true
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            builtInZoomControls = false
+            displayZoomControls = false
+        }
 
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
 
         setContentView(webView)
 
         webView.loadUrl("http://park-ban.ir/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
